e918a2dfe566294cb9e13b256cd2e9712a45a9f7
siddart r k <rk.siddart@gmail.com>
2021-04-22 09:55:16 -0400

    Initial commit

---
+++ src/main/java/com/traddis/awsimageupAndDown/AwsImageUpAndDownApplication.java
@@ -0,0 +1,13 @@
+package com.traddis.awsimageupAndDown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageUpAndDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageUpAndDownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/traddis/awsimageupAndDown/AwsImageUpAndDownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.traddis.awsimageupAndDown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageUpAndDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
